d82a8315724044748c00f56be92387ba811cacb7
yuki1996 <laetitia.boucher96@live.fr>
2017-01-18 22:50:42 +0100

    ajout des spécifications dans iGrid

--- IGrid.java
+++ IGrid.java
@@ -1,17 +1,153 @@
+package appli_info;
 import java.util.Set;
 
+/**
+ * @inv <pre>
+ *	    size() > 0
+ * 		isFull() <==> forall cell : cells(), getCell(coord).value() != 0
+ * 		getCell(coord) <==>	cells[coord.col][coord.row]
+ *      </pre>
+ * @cons <pre>
+ *     $DESC$ Une grille de taille n et avec les valeurs de départ
+ *     
+ *     $ARGS$ int n, Map map<ICoord coord, int value>
+ *     
+ *     $PRE$ 
+ *         n != null && map != null 
+ *         
+ *     $POST$ 
+ *         size() == n
+ * 		   forall coord : map, getCell(coord).value() == value
+ * 								&& cells.contains(getCell(coord))
+ *    </pre>
+ * 
+ * @cons <pre>
+ *     $DESC$ Une grille de taille n 
+ *     
+ *     $ARGS$ int n
+ *     
+ *     $PRE$ 
+ *         n != null 
+ *         
+ *     $POST$ 
+ *         size() == n
+ * 		   cells() == null
+ *    </pre>
+ */
 public interface IGrid {
+	
+	//REQUÊTE
+	/**
+	 * Retourne la taille de la grille
+	 */
 	int size();
-	Cell[][] cells();
-	Cell getCell(Coord c);
+	
+	/**
+	 * Retourne le tableau de cellule
+	 */
+	ICell[][] cells();
+	
+	/**
+	 * Retourne la cellule à coordonnée c.
+	 * @pre: <pre>
+	 * 		c != null
+	 * </pre>
+	 */
+	ICell getCell(Coord c);
+	
+	/**
+	 * Retourne si la grille est complète
+	 */
 	boolean isFull();
-	Set<Cell> getRow(Coord coord);
-	Set<Cell> getCol(Coord coord);
-	Set<Cell> getSector(Coord coord);
+	
+	/**
+	 * Retourne l'ensemble des cellules de la ligne d'où se trouve la 
+	 * coordonnée coord
+	 * @pre : <pre>
+	 * 		coord != null
+	 * </pre>
+	 */
+	Set<ICell> getRow(ICoord coord);
+	
+	/**
+	 * Retourne l'ensemble des cellules de la colonne d'où se trouve la 
+	 * coordonnée coord
+	 * @pre : <pre>
+	 * 		coord != null
+	 * </pre>
+	 */
+	Set<ICell> getCol(ICoord coord);
+	
+	/**
+	 * Retourne l'ensemble des cellules de la région d'où se trouve la 
+	 * coordonnée coord
+	 * @pre : <pre>
+	 * 		coord != null
+	 * </pre>
+	 */
+	Set<ICell> getSector(ICoord coord);
+	
+	//COMMANDE
+	
+	/**
+	 * Efface toutes les valeurs de la grille qui ne sont pas celle de
+	 * départ
+	 * @post :
+	 */
 	void reset();
+	
+	/**
+	 * Efface toutes les valeurs de la grille.
+	 * @post :<pre>
+	 * 		cells == null
+	 * </pre>
+	 */
 	void clear();
-	void changeValue(Coord coord, int value);
-	void resetValue(Coord coord, int value);
-	void addPossibility(Coord coord, int value);
-	void removePossibility(Coord coord, int value);	
+	
+	/**
+	 * Change la valeur de la cellule de coord par value.
+	 * @pre <pre>
+	 * 		coord != null
+	 * 		1 <= value <= size()
+	 * </pre>
+	 * @post <pre>
+	 * 		getCell(coord).value == value
+	 * </pre>
+	 */
+	void changeValue(ICoord coord, int value);
+	
+	/**
+	 * Réinitialise la valeur de la cellule de coord par 0.
+	 * @pre <pre>
+	 * 		coord != null
+	 * </pre>
+	 * @post <pre>
+	 * 		getCell(coord).value == 0
+	 * </pre>
+	 */
+	void resetValue(ICoord coord);
+	
+	/**
+	 * Ajoute la valeur value dans les possibilités la cellule de coord.
+	 * @pre <pre>
+	 * 		coord != null
+	 * 		1 <= value <= size()
+	 * </pre>
+	 * @post <pre>
+	 * 		getCell(coord).possibility[value]
+	 * </pre>
+	 */
+	void addPossibility(ICoord coord, int value);
+	
+	/**
+	 * Supprime la valeur value dans les possibilités la cellule de coord.
+	 * @pre <pre>
+	 * 		coord != null
+	 * 		1 <= value <= size()
+	 * </pre>
+	 * @post <pre>
+	 * 		!getCell(coord).possibility[value]
+	 * </pre>
+	 */
+	void removePossibility(ICoord coord, int value);	
 }
