80a3ec61724e80481870a7f34b870449411e119f
hardopau <paul.hardouin@etu.univ-rouen.fr>
2017-01-18 17:10:15 +0100

    Add files via upload
    j'ai ajouté IUnmodifiable, j'ai pris les coord et ICoord de mpoo1 en virant les truc du jeu d'échec et j'ai fait 2-3 modif du genre mettre un package(on pourra modifié sa plus tard) ou mettre ICell a la place de Cell dans l'interface IGrid.

--- IGrid.java
+++ IGrid.java
@@ -1,17 +1,18 @@
+package appli_info;
 import java.util.Set;
 
 public interface IGrid {
 	int size();
-	Cell[][] cells();
-	Cell getCell(Coord c);
+	ICell[][] cells();
+	ICell getCell(Coord c);
 	boolean isFull();
-	Set<Cell> getRow(Coord coord);
-	Set<Cell> getCol(Coord coord);
-	Set<Cell> getSector(Coord coord);
+	Set<ICell> getRow(ICoord coord);
+	Set<ICell> getCol(ICoord coord);
+	Set<ICell> getSector(ICoord coord);
 	void reset();
 	void clear();
-	void changeValue(Coord coord, int value);
-	void resetValue(Coord coord, int value);
-	void addPossibility(Coord coord, int value);
-	void removePossibility(Coord coord, int value);	
+	void changeValue(ICoord coord, int value);
+	void resetValue(ICoord coord, int value);
+	void addPossibility(ICoord coord, int value);
+	void removePossibility(ICoord coord, int value);	
 }
