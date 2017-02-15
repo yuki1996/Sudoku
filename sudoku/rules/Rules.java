33d0c5b7bbec0cbd78cc098b163b01bea880b00e
yuki1996 <laetitia.boucher96@live.fr>
2017-02-15 15:23:46 +0100

    Add files via upload

--- sudoku/rules/Rules.java
+++ sudoku/rules/Rules.java
@@ -1,5 +1,49 @@
 package sudoku.rules;
 
+import util.Contract;
+
 public enum Rules {
+	ONE_CANDIDATE() {
+		String describe(Report report) {
+			Contract.checkCondition(report != null);
+			return "";
+		}
+		Report generateReport() {
+			return null;
+		}
+		void execute(Report report) {
+			Contract.checkCondition(report != null);
+		}
+		
+	},
+	ONLY_CANDIDATE() {
+		String describe(Report report) {
+			Contract.checkCondition(report != null);
+			return "";
+		}
+		Report generateReport() {
+			return null;
+		}
+		void execute(Report report) {
+			Contract.checkCondition(report != null);
+		}
+		
+	},
+	PAIR_TRIPLET() {
+		String describe(Report report) {
+			Contract.checkCondition(report != null);
+			return "";
+		}
+		Report generateReport() {
+			return null;
+		}
+		void execute(Report report) {
+			Contract.checkCondition(report != null);
+		}
+		
+	};
+	abstract String describe(Report report);
+	abstract Report generateReport();
+	abstract void execute(Report report);
 
 }
--- sudoku/rules/RulesManager.java
+++ sudoku/rules/RulesManager.java
@@ -1,5 +1,34 @@
 package sudoku.rules;
 
+import sudoku.model.IGrid;
+import util.Contract;
+
 public class RulesManager {
 
+	//ATTRIBUTS
+	private IGrid grid;
+	private Report lastReport;
+	
+	
+	//CONSTRUCTEUR
+	public RulesManager(IGrid g) {
+		Contract.checkCondition(g != null);
+		grid = g;
+		lastReport = null;
+	}
+	
+	//REQUÊTES
+	public Report FindRule() {
+		Report report = new Report();
+		for (int i = 0; i < Rules.values().length && report == null; i++) {
+			report.setRule(Rules.values()[i]);
+		}
+		lastReport = report;
+		return report;
+	}
+	
+	//COMMANDES
+	public void executeReport() {
+		
+	}
 }
