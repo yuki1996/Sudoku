fe277ee8b20a3f207180738828af41406d864249
fretardloic <fretardloic@gmail.com>
2017-03-30 11:15:27 +0200

    La surbrillance se met à jour avec plus d'actions

--- sudoku/model/StdSudokuModel.java
+++ sudoku/model/StdSudokuModel.java
@@ -223,6 +223,7 @@ public class StdSudokuModel implements SudokuModel {
         Contract.checkCondition(cmd != null, "cmd est null");
         cmd.act();
         history.add(cmd);
+        ruleManager.clear();
         propertySupport.firePropertyChange(FINISH, false, isWin());
     }
     
@@ -230,12 +231,14 @@ public class StdSudokuModel implements SudokuModel {
         Contract.checkCondition(canUndo());
         history.getCurrentElement().act();
         history.goBackward();
+        ruleManager.clear();
     }
     
     public void redo() {
         Contract.checkCondition(canRedo());
         history.goForward();
         history.getCurrentElement().act();
+        ruleManager.clear();
     }
     
     public void addPropertyChangeListener(String propertyName,
